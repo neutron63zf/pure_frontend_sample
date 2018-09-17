cacfcc0a226bba6564f7f78615a58af42f72703a
Nkowne63 <neutron63zf@gmail.com>
2018-09-18 05:54:44 +0900

    fix by prettier

--- src/app/models/PostModel.ts
+++ src/app/models/PostModel.ts
@@ -1,10 +1,9 @@
 export interface PostInterface {
-    username: string,
-    timestamp: Date,
-    content: string
+  username: string
+  timestamp: Date
+  content: string
 }
 export class PostModel implements PostInterface {
-    constructor(readonly username, readonly timestamp, readonly content){
-    }
+  constructor(readonly username, readonly timestamp, readonly content) {}
 }
 export default PostModel
